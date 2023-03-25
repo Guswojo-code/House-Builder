5ae0cf87590a837e30df6aa3b58688d0b197d2ce
Guswojo-code <74015971+Guswojo-code@users.noreply.github.com>
2023-03-25 10:54:57 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,89 @@
 #include "raylib.h"
 #include "stdio.h"
 #include "unistd.h"
+#include "string.h"
+
+
+char* coord(Vector2 object)
+{
+
+    char buffer [50];
+    sprintf(buffer, "x=%d y=%d", object.x, object.y);
+
+    return buffer;
+
+}
+
+
 int main(int argc, char *argv[])
 {
   const int screenWidth = 800;
   const int screenHeight = 450;
-  Vector2 ball = { -100.0f, -100.0f };
-  ball.x = 400;
-  ball.y = 225;
+  Vector2 object = { -100.0f, -100.0f };
+  
+  object.x = 400;
+  object.y = 225;
+
   InitWindow(screenWidth, screenHeight, "house");
   SetTargetFPS(60);
+
   float acceleration = 0.000769;
-  const int speed = 1.0f;
+  const int speed = 30.0f;
+  Vector2 object1;
   int sleepTime;
   Vector2 size;
-  while (!WindowShouldClose()) {
-    
+  Vector2 block[10];
+  int blocknum = 0;
+  int i;
+  int roof = 10;
+
+  Image cat = LoadImage("objects/graybrick.png");
+  ImageFlipHorizontal(&cat);
+  ImageResize(&cat, 30, 30);
+
+  //ImageDrawRectangle(&cat, 4, 20, 10, 10, WHITE);
+
+  Texture2D texture = LoadTextureFromImage(cat);
+
+  while (!WindowShouldClose()){
+
     float deltaTime = GetFrameTime();
     sleepTime = deltaTime;
     size.x = 30;
     size.y = 30;
-    if (IsKeyDown(KEY_RIGHT)) { ball.x += speed; usleep(sleepTime);}
-    if (IsKeyDown(KEY_LEFT)) { ball.x -= speed; usleep(sleepTime); }
-    if (IsKeyDown(KEY_UP)) { ball.y -= speed; usleep(1000); }
-    if (IsKeyDown(KEY_DOWN)){ ball.y += speed; usleep(1000); }
+    if (IsKeyPressed(KEY_ENTER)) {
+      block[blocknum] = object;
+      printf("%d", blocknum);
+      if (roof > blocknum){
+        blocknum ++;
+      }
+    }
+    if (IsKeyPressed(KEY_RIGHT)) { object.x += speed; }
+    if (IsKeyPressed(KEY_LEFT)) { object.x -= speed; }
+    if (IsKeyPressed(KEY_UP)) { object.y -= speed; }
+    if (IsKeyPressed(KEY_DOWN)){ object.y += speed;}
+
     //acceleration += 0.000125;
 
     //ball = GetMousePosition();
     BeginDrawing();
+
     ClearBackground(RAYWHITE);
-    DrawRectangleV(ball, size, GREEN);
+    //DrawRectangleV(object, size, GREEN);
+
+    for (i = 0; i < blocknum; i++) {
+          DrawTextureV(texture, block[i], WHITE);
+    }
+    DrawTextureV(texture, object, WHITE);
+    DrawRectangleLines(object.x, object.y, size.x, size.y, BLACK);
+    
+    DrawText(TextFormat("Y = %03i", object.y), screenWidth/2, screenHeight/2, 10, BLACK);
+
     EndDrawing();
   }
+  UnloadImage(cat);
   CloseWindow();
   return 0;
 }
+
+
